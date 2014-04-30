542078c78379c5ed1ba0d931f76b6ae306cbb4b6
stujo <stujo@imagescript.com>
2014-04-30 11:33:25 -0700

    rails new jquery_intro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JqueryIntro::Application.config.secret_key_base = 'a1197a4cd6d98b1757d6767ac54da2a10ee7bc20bbac7f5689553b890724f7082a3eebd94a2a52960ce401b6cb9ff94cfaf80487b2073a29f02d2f8d3ce7a244'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JqueryIntro::Application.config.session_store :cookie_store, key: '_jquery_intro_session'
