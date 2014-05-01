a75e9ec7b495ffb3f8d56c305c524fcaf77c6858
stujo <stujo@imagescript.com>
2014-05-01 00:14:28 -0700

    Tweaking carousel

--- app/assets/example_code/real_world_examples/carousel.js
+++ app/assets/example_code/real_world_examples/carousel.js
@@ -5,9 +5,8 @@ $(document).ready(
   function () {
     $('#my_carousel').slick(
       {
-        autoplay: true,
-        autoplaySpeed: 1000,
-        speed: 500
+        speed: 500,
+        draggable: false
       }
     );
   }
